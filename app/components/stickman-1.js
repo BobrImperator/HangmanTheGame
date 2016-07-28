c1b65e26869b89bf2eeec0bdbf9bd8c2e5fa95c4
BobrImperator <lordboberro@gmail.com>
2016-07-28 17:33:18 +0200

    Hangman done! ... almost , but playable

--- app/components/stickman-1.js
+++ app/components/stickman-1.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-playGame: Ember.inject.service('play-game')
-
+  playGame: Ember.inject.service('play-game')
 });
--- tests/integration/components/pick-hangman-test.js
+++ tests/integration/components/pick-hangman-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('stick-gallow', 'Integration | Component | stick gallow', {
+moduleForComponent('pick-hangman', 'Integration | Component | pick hangman', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{stick-gallow}}`);
+  this.render(hbs`{{pick-hangman}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#stick-gallow}}
+    {{#pick-hangman}}
       template block text
-    {{/stick-gallow}}
+    {{/pick-hangman}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
